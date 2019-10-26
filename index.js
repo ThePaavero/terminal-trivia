83f38e3681cadc7d97fe70f647dd95a42ce10108
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-10-26 23:53:09 +0300

    Database fix + boilerplate logic.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const prompt = require('prompts')
-const _ = require('lodash')
+const prompts = require('prompts')
 const fs = require('fs')
 require('colors')
 
-const data = JSON.parse(fs.readFileSync('database.json'))
-const shuffledQuestions = _.shuffle(data)
+const getRandomQuestionObject = (data) => {
+  return data[Math.round(Math.random() * data.length - 1)]
+}
 
-console.log(shuffledQuestions)
+const promptAnswerForQuestion = async (question) => {
+  return await prompts({
+    type: 'text',
+    name: 'answer',
+    message: question.q,
+  })
+}
+
+const init = () => {
+  console.clear()
+  console.log(`Welcome to Terminal Trivia!`.bgWhite.black)
+  promptAnswerForQuestion(getRandomQuestionObject(JSON.parse(fs.readFileSync('database.json'))))
+}
+
+init()
