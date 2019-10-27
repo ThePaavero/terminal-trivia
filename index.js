e312e2002c32c9496402301df50dbd745ba2a2a8
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-10-27 08:26:15 +0200

    WIP

--- index.js
+++ index.js
@@ -15,12 +15,36 @@ const promptAnswerForQuestion = async (question) => {
   return answerObject.answer
 }
 
+const doOnCorrectAnswer = (answer) => {
+  console.log(`${answer} is absolutely correct!`.green)
+}
+
+const doOnIncorrectAnswer = (correctAnswer) => {
+  // ...
+  console.log(`Wrong, sorry!`.red + `\nThe correct answer would've been "${correctAnswer}"`.gray)
+}
+
+const userAnswerIsCorrect = (userAnswer, correctAnswer) => {
+  // @todo Do better than this, the data is pretty crappy.
+  /*
+  Ideas:
+    - If the correct answer starts with "The ", remove it and compare again.
+    - Sometimes the correct answers are words instead of numbers, like "Eleven." When the answer is numerical we want to accept both textual version and actual numbers.
+  */
+  return userAnswer.trim().toLowerCase() === correctAnswer.trim().toLowerCase()
+}
+
 const doRound = async () => {
+  console.clear()
   const questionObject = getRandomQuestionObject(data)
   const userAnswer = await promptAnswerForQuestion(questionObject)
   const correctAnswer = questionObject.a
-  console.log(userAnswer)
-  console.log(correctAnswer)
+  if (userAnswerIsCorrect(userAnswer, correctAnswer)) {
+    doOnCorrectAnswer(userAnswer)
+  } else {
+    doOnIncorrectAnswer(correctAnswer)
+  }
+  setTimeout(doRound, 2000)
 }
 
 const init = () => {
